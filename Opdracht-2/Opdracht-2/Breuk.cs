eb3c66b5372a6a6b3346db184cff38a40e3b6a2c
femketrampo <76632910+femketrampo@users.noreply.github.com>
2022-03-22 09:46:55 +0100

    Add files via upload

---
+++ Opdracht-2/Opdracht-2/Breuk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Opdracht_2
+{
+    class Breuk
+    {
+        public double Teller;
+        public double Noemer;
+
+        public Breuk()
+        {
+            Teller = 4;
+            Noemer = 4;
+        }
+        public Breuk(double teller, double noemer)
+        {
+            Teller = teller;
+            Noemer = noemer;
+        }
+    }
+}
